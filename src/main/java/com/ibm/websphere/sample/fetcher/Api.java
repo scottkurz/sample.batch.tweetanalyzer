bf1e8a6c7c8a061190051917505f772b3d7cd359
Scott Kurz <skurz@us.ibm.com>
2018-09-25 19:24:29 -0400

    Rename fetcher to com.ibm.websphere package

--- src/main/java/com/ibm/websphere/sample/fetcher/Api.java
+++ src/main/java/com/ibm/websphere/sample/fetcher/Api.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package application.fetcher;
+package com.ibm.websphere.sample.fetcher;
 
 
 import javax.ws.rs.core.Application;
--- src/main/java/com/ibm/websphere/sample/fetcher/CorsFilter.java
+++ src/main/java/com/ibm/websphere/sample/fetcher/CorsFilter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package application.fetcher;
+package com.ibm.websphere.sample.fetcher;
 
 import java.io.IOException;
 
