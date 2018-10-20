99c017df65d9d795544224e165c94d6ca17b0c4a
Scott Kurz <skurz@us.ibm.com>
2018-10-19 23:14:01 -0400

    Revive CorsFilter, but disabled (commented-out) by default.

---
+++ src/main/java/com/ibm/websphere/sample/fetcher/CorsFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 International Business Machines Corp.
+ * 
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership. Licensed under the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.websphere.sample.fetcher;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+// 
+// Commented out initially to provide tighter security out of the box by default.
+// Included as sample (commented-out) code.  May in particular be useful in case 
+// you want to enable the "create-react-app development" server for development 
+// of the JavaScript UI layer.
+//
+
+//@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+   @Override
+   public void filter(final ContainerRequestContext requestContext,
+                      final ContainerResponseContext cres) throws IOException { }
+
+    /*
+   @Override
+   public void filter(final ContainerRequestContext requestContext,
+                      final ContainerResponseContext cres) throws IOException {
+      cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+      cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+      cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+      cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+      cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+   }
+   */
+
+}
